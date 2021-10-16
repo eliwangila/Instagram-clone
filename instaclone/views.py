5bab526a21595f2f2eb606f381a825ac672842e6
eliwangila <eli.wangila@student.moringaschool.com>
2021-10-16 17:56:58 +0300

    add def add like comment

--- instaclone/views.py
+++ instaclone/views.py
@@ -52,3 +52,18 @@ def add_comment(request):
         if description is not None:
             Comment.objects.create(post_linked=post, description=description, user=request.user)
             return redirect(reverse('home'))
+ 
+def like_post(request, postid):
+    post = Post.objects.get(id=postid)
+    try:
+        is_Liked = Like.objects.get(post_linked=post, user__username=request.user.username)
+        Like.objects.filter(post_linked=post, user__username=request.user.username).delete()
+        post.likes -= 1
+    except Like.DoesNotExist:
+
+        Like.objects.create(post_linked=post, user=request.user)
+        post.likes += 1
+    post.save()
+    return redirect(reverse('home'))
+
+           
