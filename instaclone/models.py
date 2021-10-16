9803d5fe836ca97b0b297001b11ef5f1d4fcf125
eliwangila <eli.wangila@student.moringaschool.com>
2021-10-16 17:43:48 +0300

    add register

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-# Create your models here.
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -13,12 +11,13 @@ class Profile(models.Model):
     biography = models.TextField(blank=True)
     phone_number = models.CharField(max_length=20, blank=True)
     profile_pic = models.ImageField(blank=True, null=True)
-    
+
     def __str__(self):
         return self.user.username
 
     def split_biography(self):
         return self.biography.split("\n")
+
 
 class Post(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
@@ -34,9 +33,11 @@ class Post(models.Model):
     def __str__(self):
         return self.caption
 
+
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_linked = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post_linked = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
     description = models.CharField(max_length=500)
     comment_posted_on = models.DateTimeField(default=timezone.now)
 
@@ -45,11 +46,13 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ('-comment_posted_on',)
+
+
 class Like(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post_linked = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts')
+    post_linked = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
-        return 'User :{} Liked {} Post '.format(self.user.username,self.post_linked.caption)
-
+        return 'User :{} Liked {} Post '.format(self.user.username, self.post_linked.caption)
