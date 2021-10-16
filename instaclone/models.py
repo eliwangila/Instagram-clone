3a550fc80677b6adcebb76b54f5339ee7fe74140
eliwangila <eli.wangila@student.moringaschool.com>
2021-10-16 17:39:33 +0300

    add string representation

--- instaclone/models.py
+++ instaclone/models.py
@@ -13,3 +13,9 @@ class Profile(models.Model):
     biography = models.TextField(blank=True)
     phone_number = models.CharField(max_length=20, blank=True)
     profile_pic = models.ImageField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.user.username
+
+    def split_biography(self):
+        return self.biography.split("\n")
