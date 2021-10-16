b4c6c7cdf2798bd45ee6a7a6d99213f6bf7db287
eliwangila <eli.wangila@student.moringaschool.com>
2021-10-16 17:38:13 +0300

    add models class profile

--- instaclone/models.py
+++ instaclone/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    biography = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    profile_pic = models.ImageField(blank=True, null=True)
