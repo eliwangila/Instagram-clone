f1e73771a69402e37d73c8ebb7f4fc37fc000246
eliwangila <eli.wangila@student.moringaschool.com>
2021-10-16 17:58:48 +0300

    add def add posts

--- instaclone/forms.py
+++ instaclone/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from django.forms import ModelForm
+from django.http.response import HttpResponse
+from django.shortcuts import redirect
+from django.template import loader
+from django.urls.base import reverse
 
 from .models import Post, Profile
 
@@ -20,3 +24,23 @@ class ProfileForm(forms.Form):
     last_name = forms.CharField(max_length=500, required=False)
     phone_number = forms.CharField(max_length=20, required=False)
     biography = forms.CharField(required=False, widget=forms.Textarea())
+
+
+def add_post(request):
+    template = loader.get_template('insta/post.html')
+    profile = Profile.objects.get(user=request.user)
+    if request.method == "POST":
+        profile = Profile.objects.get(user=request.user)
+        form = PostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            fs = form.save(commit=False)
+            fs.author = profile
+            fs.save()
+            return redirect(reverse('home'))
+    else:
+        form = PostForm()
+        pass
+
+    context = {'form': form ,'profile':profile}
+    return HttpResponse(template.render(context, request))
